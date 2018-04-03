c9bc536f63c5a04c7a6099d8328720b0d7e2a155
Paddy Cremen <cremenp@tcd.ie>
2018-04-03 11:16:03 +0000

    remove original map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,33 +1,16 @@
- function initMap() {
-     var map = new google.maps.Map(document.getElementById("map"), {
-         zoom: 3,
-         center: {
-             lat: 46.619261,
-             lng: -33.134766
-         }
-     });
- };
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
  
  
  //Search bar. 
- //Code from: https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+ // Orginal code from: https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
  
    function initAutocomplete() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.8688, lng: 151.2195},
-          zoom: 13,
+           zoom: 15,
+         center: {
+             lat: 53.3498053,
+             lng: -6.2603097
+         },
           mapTypeId: 'roadmap'
         });
 
