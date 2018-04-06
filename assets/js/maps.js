9678e94cd100234ac02ce03b6e21bfa6c6a1b30d
Paddy <cremenp@tcd.ie>
2018-04-06 13:42:54 +0100

    add filter button functionality

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -17,6 +17,60 @@ var activities = "streetart,racetracks,sportsteams,theater,opera,museums,festiva
 
 var accommodation  = "guesthouses,campgrounds,hostels,hotels";
 
+function createSearchBar(map) {
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function () {
+    searchBox.setBounds(map.getBounds());
+  });
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function () {
+    var places = searchBox.getPlaces();
+    //if (places.length == 0) {
+    //return;
+    // }
+    // Clear out the old markers.
+    markers.forEach(function (marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function (place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      }
+      else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
 
 var generateNewMap = function (latitude, longitude) {
   return new google.maps.Map(document.getElementById('map'), {
@@ -57,14 +111,20 @@ var yelpResponse = {};
 var locations = [];
 
 
+
+
+
 testYelpApi(originalLat, originalLng, activities,  function(data){
   yelpResponse = data;
   pushToLocations();
   pushToCards();
+ 
+
   // initAutocomplete();
-  restOfMaps(activities);
 
 });
+
+
 
 
 let pushToCards = function(){
@@ -107,92 +167,77 @@ let pushToLocations = function () {
 
 
 
- //Search bar. 
- // Orginal code from: https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
  
    function initMap() {
        
     var map = generateNewMap(originalLat, originalLng);
 
+    restOfMaps(activities, map);
+     createSearchBar(map);
 
 
       };
  
 
-      function restOfMaps (filterTerm){
-        var map = generateNewMap(originalLat, originalLng);
 
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+      function restOfMaps (filterTerm, map){
+      //  var map = generateNewMap(originalLat, originalLng);
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
+      
+      $(".foodAndDrinkButton").click(function(){
+        addYelpMarkers(map, foodAndDrink, marker);
+       });
 
-        var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       $(".activitiesButton").click(function(){
+        addYelpMarkers(map, activities, marker);
+       });
 
-   
+       $(".accommodationButton").click(function(){
+        addYelpMarkers(map, accommodation, marker);
+       });
+       
+
       var marker, i; 
 
-      
+      map.addListener("tilesloaded", function () {
 
-
-
-
-    map.addListener("tilesloaded",  function() {
-  
-  
-        var newPosition = map.getCenter();
-  
-        var newLat = newPosition.lat();
-        var newLng = newPosition.lng();
-       
-  
-        testYelpApi(newLat, newLng, filterTerm, function (data) {
-            
-  
-          yelpResponse = data;
-          pushToLocations();
-          pushToCards();
-          // initAutocomplete();
-          
-  
-          for (i = 0; i < locations.length; i++) {
-  
-  
-            marker = new google.maps.Marker({
-              position: locations[i],
-              map: map,
-        
-            })
-            let yelpObject = JSON.stringify(yelpResponse.businesses[i]);
-            marker.addListener('click', function () {
-              $("#onClickContent").html(yelpObject);
-        
-              /*map.setZoom(20);
-              map.setCenter(marker.getPosition());*/
-            });
-          };
-        
-  
-        });
-  
-  
+   addYelpMarkers(map, filterTerm, marker);
       });
-
-
-
-
-
-
-
-    
-      
-      
+  
+}
  
+
+
+
+
+
+
+
+
+function addYelpMarkers(map, filterTerm, marker) {
+ 
+    var newPosition = map.getCenter();
+    var newLat = newPosition.lat();
+    var newLng = newPosition.lng();
+    testYelpApi(newLat, newLng, filterTerm, function (data) {
+      yelpResponse = data;
+      pushToLocations();
+      pushToCards();
+      // initAutocomplete();
+      for (let i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+          position: locations[i],
+          map: map,
+        });
+        let yelpObject = JSON.stringify(yelpResponse.businesses[i]);
+        marker.addListener('click', function () {
+          $("#onClickContent").html(yelpObject);
+        });
+      }
+      ;
+    });
+  };
+
        /* var yelpMarkers = locations.map(function(location, i) {
             return new google.maps.Marker({
                 position: location,
@@ -204,73 +249,3 @@ let pushToLocations = function () {
         }); */
 
      
-
-/*
-//my code for adding click events to marker clustrt
-google.maps.event.addListener(markerCluster, 'clusterclick', function(cluster) {
-  map.setZoom(50);
-  map.setCenter(marker.getPosition());
-});
-      
-*/
-
-
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
-
-          if (places.length == 0) {
-            return;
-          }
-
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
-
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-
-
-          
-
-            
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-          
-          
-
-        });
-      }
- 
