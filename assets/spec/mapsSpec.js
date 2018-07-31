32c282f09814ad7be10b4de983a0f75338116e3b
Paddy Cremen <cremenp@tcd.ie>
2018-07-31 17:37:30 +0000

    fix bug in Jasmine testing where 'it' block runs before asynchronous function has finished

--- assets/spec/mapsSpec.js
+++ assets/spec/mapsSpec.js
@@ -1,159 +1,176 @@
-describe('checkIfOnMobile function', function () {
+describe('checkIfOnMobile function', function() {
 
-  it("should exist", function () {
+  it("should exist", function() {
     expect(checkIfOnMobile).toBeDefined();
-  })
-  it('should not return a value if not on mobile device', function () {
+  });
+
+  it('should not return a value if not on mobile device', function() {
     let result = checkIfOnMobile();
-    expect(result).toBeUndefined()
-  })
-})
+    expect(result).toBeUndefined();
+  });
+});
 
-describe('generateNewMap function', function () {
-  it("should exist", function () {
+describe('generateNewMap function', function() {
+  it("should exist", function() {
     expect(generateNewMap).toBeDefined();
-  })
-})
+  });
+});
 
-describe("getYelpData function", function () {
+describe("getYelpData function", function() {
+
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  let result = "an initial value";
   //enables testing of asynchronous functions
+  // postpones running of 'it' functions 
   //code source: https://metabroadcast.com/blog/asynchronous-testing-with-jasmine 
-  beforeEach(function (done) {
-    setTimeout(function () {
+  beforeEach(function(done) {
+    setTimeout(function() {
       result = 'a different value';
       done();
-    }, 2500);
+    }, 6000);
   });
 
-  it("should exist", function () {
+  it("should exist", function() {
     expect(getYelpData).toBeDefined();
-  })
-
-  let businessesLength = 50;
-  let latitude = 53.3498053
-  let longitude = -6.2603097
-  let searchQuery = "food,bars";
-
-  getYelpData(latitude, longitude, searchQuery, function (data) {
-
-    yelpResponse = data
-    businessesLength = yelpResponse.businesses.length
-  })
-  it('should initially return 20 businesses ', function () {
-    expect(businessesLength).toBe(20)
   });
-})
 
-describe("ifUndefinedReturnNA function", function () {
-  it("should exist", function () {
+  let latitude = 53.3498053;
+  let longitude = -6.2603097;
+  let searchQuery = "food,bars";
+  let businessesLength = 50;
+
+
+  getYelpData(latitude, longitude, searchQuery, function(data) {
+    let yelpResponse = data;
+    businessesLength = yelpResponse.businesses.length;
+  });
+
+
+  it('should initially return 20 businesses ', function() {
+    expect(businessesLength).toBe(20);
+  });
+});
+
+
+describe("ifUndefinedReturnNA function", function() {
+  it("should exist", function() {
     expect(ifUndefinedReturnNA).toBeDefined();
-  })
+  });
 
-  it("should return 'N/A' when undefined entered  ", function () {
-    let result = ifUndefinedReturnNA(undefined)
-    expect(result).toBeDefined()
-    expect(result).toBe('N/A')
-  })
+  let firstResult = ifUndefinedReturnNA(undefined);
+  it("should return 'N/A' when undefined entered  ", function() {
+    expect(firstResult).toBeDefined();
+    expect(firstResult).toBe('N/A');
+  });
 
-  it("should return argument if argument is not undefined ", function () {
-    let argument = "argument"
-    let result = ifUndefinedReturnNA(argument)
-    expect(result).toBe(argument)
-  })
-})
+  let definedArgument = "test argument";
+  let secondResult = ifUndefinedReturnNA(definedArgument);
+  it("should return argument if argument is not undefined ", function() {
+    expect(secondResult).toBe(definedArgument);
+  });
+});
 
-describe("showAllCategories function", function () {
-  it("should exist", function () {
+describe("showAllCategories function", function() {
+  it("should exist", function() {
     expect(showAllCategories).toBeDefined();
   })
 
-  it("should return an array the same length as its argument ", function () {
-    let sampleArray = [{
-        alias: "desserts",
-        title: "Desserts"
-      },
-      {
-        alias: "tea",
-        title: "Tea Rooms"
-      },
-      {
-        alias: "breakfast_brunch",
-        title: "Breakfast & Brunch"
-      }
-    ]
+  let sampleArray = [{
+      alias: "desserts",
+      title: "Desserts"
+    },
+    {
+      alias: "tea",
+      title: "Tea Rooms"
+    },
+    {
+      alias: "breakfast_brunch",
+      title: "Breakfast & Brunch"
+    }
+  ];
 
-    let sampleArrayLength = sampleArray.length
-    let resultArray = showAllCategories(sampleArray)
-    let resultArrayLength = resultArray.length
-    expect(resultArrayLength).toBe(sampleArrayLength)
-  })
-})
+  let sampleArrayLength = sampleArray.length;
+  let resultArray = showAllCategories(sampleArray);
+  let resultArrayLength = resultArray.length;
+  it("should return an array the same length as its argument ", function() {
+    expect(resultArrayLength).toBe(sampleArrayLength);
+  });
+});
 
 
-describe("pushToCardsOrInfoboxes function", function () {
+describe("pushToCardsOrInfoboxes function", function() {
+
+
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  let result = "an initial value"
   //enables testing of asynchronous functions
   //code source: https://metabroadcast.com/blog/asynchronous-testing-with-jasmine 
-  beforeEach(function (done) {
-    setTimeout(function () {
+  beforeEach(function(done) {
+    setTimeout(function() {
       result = 'a different value';
       done();
-    }, 3500);
+    }, 5000);
   });
 
-  it("should exist", function () {
+
+  getYelpData(53.3498053, -6.2603097, 'food, bars', function(data) {
+    let yelpResponse = data;
+    pushToCardsOrInfoboxes(map, yelpResponse);
+  });
+
+  it("should exist", function() {
     expect(pushToCardsOrInfoboxes).toBeDefined();
   })
 
-  let businessesLength = 50;
-  let latitude = 53.3498053
-  let longitude = -6.2603097
-  let searchQuery = "food,bars";
-  let yelpResponse = {}
-let  map = ""
-  getYelpData(latitude, longitude, searchQuery, function (data) {
+  it("should add aside cards to #cards-content on non-mobile devices ", function() {
+    let cardsContent = $("#cards-content").html();
+    expect(cardsContent).toContain('  <div class="card aside-card card-0">');
+  });
+});
 
-    yelpResponse = data
-    pushToCardsOrInfoboxes(map, yelpResponse)
-
-    it("should add aside cards to #cards-content on non-mobile devices ", function () {
-      let cardsContent = $("#cards-content").html()
-      expect(cardsContent).toContain('  <div class="card aside-card card-0">')
-    })
-  })
-})
-
-describe("pushToLocations function", function () {
+describe("pushToLocations function", function() {
   //enables testing of asynchronous functionsx
   //code source: https://metabroadcast.com/blog/asynchronous-testing-with-jasmine 
-  beforeEach(function (done) {
-    setTimeout(function () {
+  let result = 'an initial value';
+  beforeEach(function(done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    setTimeout(function() {
       result = 'a different value';
       done();
-    }, 4000);
+    }, 7000);
   });
 
-  let latitude = 53.3498053
-  let longitude = -6.2603097
-  let searchQuery = "food,bars";
-  let yelpResponse = {}
-  firstLocation = {}
 
-
-  it("should exist", function () {
+  it("should exist", function() {
     expect(pushToLocations).toBeDefined();
-  })
+  });
 
-  getYelpData(latitude, longitude, searchQuery, function (data) {
 
-    yelpResponse = data
-    let locations = pushToLocations(yelpResponse)
-    let firstLocation= locations[0]
-    let latType =  typeof firstLocation["lat"]
+  let getLocations = function() {
 
-    it("should return an object with lat and lng int values", function(){
-      expect(Object.keys(firstLocation)[0]).toBe("lat")
-      expect(Object.keys(firstLocation)[1]).toBe("lng")
-      expect(latType).toBe("number")
-  })
-    })
-})
+    let latitude = 53.3498053;
+    let longitude = -6.2603097;
+    let searchQuery = "food,bars";
+    let yelpResponse = {};
+    firstLocation = {};
+
+    getYelpData(latitude, longitude, searchQuery, function(data) {
+
+      yelpResponse = data;
+      let locations = pushToLocations(yelpResponse);
+      return locations;
+    });
+  };
+
+  locations = getLocations()
+
+
+  it("should return an object with lat and lng int values", function() {
+    let firstLocation = locations[0];
+    let latType = typeof firstLocation["lat"];
+    expect(Object.keys(firstLocation)[0]).toBe("lat");
+    expect(Object.keys(firstLocation)[1]).toBe("lng");
+    expect(latType).toBe("number")
+  });
+
+});
